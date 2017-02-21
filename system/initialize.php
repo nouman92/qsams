834b4c3a85240f0f8f3e7f1f2f10abdf92b39ce1
Nouman Anjum <nouman92@live.com>
2017-02-21 11:38:00 +0500

    live url

--- system/initialize.php
+++ system/initialize.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/orm/ActiveRecord.php';
 ActiveRecord\Config::initialize(function($cfg)
 {
     $cfg->set_model_directory(__DIR__ . '/models');
-    $cfg->set_connections(array('development' => 'mysql://root:nouman92@127.0.0.1:3307/qaams'));
+    $cfg->set_connections(array('development' => 'mysql://nouman92:nouman92@noumananjum.c4wrdesrpkah.ap-south-1.rds.amazonaws.com:3307/qsams'));
 });
 ?>

a439135a8ecc14ed2f6141eb5f87cc7e8e921277
Nouman Anjum <nouman92@live.com>
2017-02-21 18:01:29 +0500

    -port update

--- system/initialize.php
+++ system/initialize.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/orm/ActiveRecord.php';
 ActiveRecord\Config::initialize(function($cfg)
 {
     $cfg->set_model_directory(__DIR__ . '/models');
-    $cfg->set_connections(array('development' => 'mysql://nouman92:nouman92@noumananjum.c4wrdesrpkah.ap-south-1.rds.amazonaws.com:3307/qsams'));
+    $cfg->set_connections(array('development' => 'mysql://nouman92:nouman92@noumananjum.c4wrdesrpkah.ap-south-1.rds.amazonaws.com:3306/qsams'));
 });
 ?>
